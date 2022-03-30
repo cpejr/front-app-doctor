9778089c7352b37ed86d518ef40156d28327111d
Maria <mariamsilva15@ufmg.br>
2022-03-30 16:04:25 -0300

    feat: criação componentes e estilização

--- src/Pages/Cadastro/Cadastro.js
+++ src/Pages/Cadastro/Cadastro.js
@@ -1,11 +1,57 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, TextInput, TouchableOpacity, Image } from "react-native";
+import logoGuilherme from "./../../assets/logoGuilherme.png";
+import {
+  Body,
+  CaixaTitulo,
+  Logo,
+  Titulo,
+  CaixaInputs,
+  StyledTextInput,
+  CaixaInputsMesmaLinha,
+  CaixaBotoes,
+  Botao
+} from "./Styles"
 
 function Cadastro() {
   return (
-    <View>
-      <Text>Cadastro</Text>
-    </View>
+    <Body>
+      <CaixaTitulo>
+        <Logo
+        source={logoGuilherme}
+        />
+        <Titulo>
+          Faça seu Cadastro
+        </Titulo>
+      </CaixaTitulo>
+
+      <CaixaInputs>
+        <StyledTextInput
+        placeholder="Nome Completo"
+        keyboardType="default"
+        />
+        <CaixaInputsMesmaLinha>
+          <StyledTextInput
+          placeholder="Telefone"
+          keyboardType="numeric"
+          />
+          <StyledTextInput
+          placeholder="Data de Nascimento"
+          keyboardType="numeric"
+          />
+        </CaixaInputsMesmaLinha>
+      </CaixaInputs>
+      
+      <CaixaBotoes>
+        <Botao
+        title="CANCELAR"
+        />
+        <Botao
+        title="CADASTRAR"
+        />
+      </CaixaBotoes>
+
+    </Body>
   );
 }
 
---
+++ src/Pages/Cadastro/Styles.js
@@ -0,0 +1,59 @@
+import styled from "styled-components/native";
+
+export const Body = styled.View`
+    display: flex;
+    align-items:center;
+    width: 100%;
+    height: 100%;
+    /* background-color: #e5e5e5 */
+`
+
+export const CaixaTitulo = styled.View`
+
+`
+
+export const Logo = styled.Image`
+    width:50;
+    height:50;
+`
+
+export const Titulo = styled.View`
+
+`
+
+export const CaixaInputs = styled.View`
+    width:100%;
+    padding-left: 10%;
+    padding-right: 10%;
+    height:auto;
+    bottom: 0;
+    background-color: #fff;
+    align-items: center;
+`
+
+export const StyledTextInput = styled.TextInput`
+    width: 100%;
+    height: 40;
+    background-color: #E4E6F4;
+    border-radius: 5;
+    padding-left: 10;
+    margin-top: 6;
+    margin-bottom: 6;
+    border: #151B57;
+    border-width: 1.5;
+`
+
+export const CaixaInputsMesmaLinha = styled.View`
+    display:flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width:100%
+`
+export const CaixaBotoes = styled.View`
+    width:100%;
+`
+
+export const Botao = styled.Button`
+
+`
+
