a0b4a30721f454571a2998a435015660df38b6b8
Adrianus Alexsander <adrianusvieira@cpejr.com.br>
2022-04-05 12:32:27 -0300

    feat: organizando botoes de teste

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,13 +1,49 @@
 import React from "react";
 import { Button, Text, View } from "react-native";
 
-function Home({navigation}) {
+function Home({ navigation }) {
   return (
     <View>
       <Text>Home</Text>
       <Button
         title="Go to Cadastro"
         onPress={() => navigation.navigate("Cadastro")}
+      />
+      <Button
+        title="Go to Login"
+        onPress={() => navigation.navigate("Login")}
+      />
+      <Button
+        title="Go to Comentarios"
+        onPress={() => navigation.navigate("ComentariosScreen")}
+      />
+      <Button
+        title="Go to Emergencia"
+        onPress={() => navigation.navigate("EmergenciaScreen")}
+      />
+      <Button
+        title="Go to GrupoAMIE"
+        onPress={() => navigation.navigate("GrupoAMIEScreen")}
+      />
+      <Button
+        title="Go to Perfil"
+        onPress={() => navigation.navigate("PerfilScreen")}
+      />
+      <Button
+        title="Go to Recomendacoes"
+        onPress={() => navigation.navigate("RecomendacoesScreen")}
+      />
+      <Button
+        title="Go to SobreMim"
+        onPress={() => navigation.navigate("SobreMimScreen")}
+      />
+      <Button
+        title="Go to AlterarDados"
+        onPress={() => navigation.navigate("AlterarDadosScreen")}
+      />
+      <Button
+        title="Go to AlterarSenha"
+        onPress={() => navigation.navigate("AlterarSenhaScreen")}
       />
     </View>
   );
