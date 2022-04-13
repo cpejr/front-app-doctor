79fb069171bc1531dd3e3f3a5a32c82d210be015
Adrianus Alexsander <adrianusvieira@cpejr.com.br>
2022-04-13 15:51:59 -0300

    feat: botoes para todas as paginas

--- src/Pages/Consultas/Consultas.js
+++ src/Pages/Consultas/Consultas.js
@@ -1,10 +1,26 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
 
-function  Consultas() {
+function Consultas({ navigation }) {
   return (
     <View>
       <Text> Consultas</Text>
+      <Button
+        title="Go to Cadastro"
+        onPress={() => navigation.navigate("Cadastro")}
+      />
+      <Button
+        title="Go to Login"
+        onPress={() => navigation.navigate("Login")}
+      />
+      <Button
+        title="Go to RecomendacoesPreConsulta"
+        onPress={() => navigation.navigate("RecomendacoesPreConsultaScreen")}
+      />
+      <Button
+        title="Go to SolicitarConsulta"
+        onPress={() => navigation.navigate("SolicitarConsultaScreen")}
+      />
     </View>
   );
 }
--- src/Pages/Exames/Exames.js
+++ src/Pages/Exames/Exames.js
@@ -1,10 +1,26 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
 
-function Exames() {
+function Exames({ navigation }) {
   return (
     <View>
       <Text>Exames</Text>
+      <Button
+        title="Go to Cadastro"
+        onPress={() => navigation.navigate("Cadastro")}
+      />
+      <Button
+        title="Go to Login"
+        onPress={() => navigation.navigate("Login")}
+      />
+      <Button
+        title="Go to FormaPagamento"
+        onPress={() => navigation.navigate("FormaPagamentoScreen")}
+      />
+      <Button
+        title="Go to SolicitarExame"
+        onPress={() => navigation.navigate("SolicitarExameScreen")}
+      />
     </View>
   );
 }
