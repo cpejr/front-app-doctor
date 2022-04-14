2873cebb5006f8696c9d1c6393e70ffd5ad3fbdc
Maria <mariamsilva15@ufmg.br>
2022-04-14 17:32:17 -0300

    feat: criação de componentes e estilização

---
+++ src/Pages/Perfil/Styles.js
@@ -0,0 +1,98 @@
+import styled from "styled-components/native";
+
+export const Body = styled.View`
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    background-color: #fff;
+    padding-left: 10%;
+    padding-right: 10%;
+`
+
+export const CaixaBotao = styled.View`
+    display: flex;
+    align-items: center;
+    width: 100%;
+    margin-bottom: 8%;
+    margin-top:6%;
+`
+
+export const CaixaViews = styled.View`
+display: flex;
+    align-items: center;
+    width: 100%;
+`
+    
+
+
+export const ViewFotoNome = styled.View`
+display: flex;
+align-items: center;
+justify-content: center;
+width: 100%;
+background-color: #F7F7F7;
+border: 2px solid #5A5D76;
+border-radius: 4px;
+padding-top: 30px;
+padding-bottom: 5px;
+margin-bottom: 5%;
+
+
+`
+
+export const Foto = styled.Image`
+width:100px;
+height:100px;
+margin-bottom: 20px;
+`
+
+export const Nome = styled.Text`
+margin-bottom: 20px;
+font-size: 20px;
+color:#151B57;
+font-weight: 550;
+`
+
+export const ViewContatoEndereco = styled.View`
+display: flex;
+align-items: start;
+justify-content: center;
+padding-left: 8%;
+width: 100%;
+background-color: #F7F7F7;
+border: 2px solid #5A5D76;
+border-radius: 4px;
+padding-top: 25px;
+padding-bottom: 5px;
+margin-bottom: 5%;
+
+`
+
+export const Titulo = styled.Text`
+margin-bottom: 20px;
+font-size: 20px;
+color:#151B57;
+font-weight: 500;
+`
+
+export const Dados = styled.Text`
+color:#151B57;
+font-weight: 450;
+margin-bottom: 15px;
+`
+
+export const CaixaBotoes = styled.View`
+    display: flex;
+    align-items: center;
+    width: 100%;
+    margin-bottom: 8%;
+    
+`
+
+export const ExcluirConta = styled.Text`
+color:#151B57;
+font-weight: 450;
+margin-top: 2%;
+text-decoration: underline;
+`
