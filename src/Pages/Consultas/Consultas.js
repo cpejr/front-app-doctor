ecd81167e9d2cb6fac4211c95bba9ebdf6f46015
Adrianus Alexsander <adrianusvieira@cpejr.com.br>
2022-04-27 01:31:49 -0300

    feat: estilizando

--- src/Pages/Consultas/Consultas.js
+++ src/Pages/Consultas/Consultas.js
@@ -11,9 +11,12 @@ import {
   CaixaHora,
   ConteudoCaixa,
   Icone,
+  BotaoView,
+  BotaoIcone,
+  BotaoCaixa,
 } from "./Styles";
-import { Botao, ConteudoBotao } from "../../styles/Botao";
-
+import ConteudoBotao from "./../../styles/ConteudoBotao";
+import botaoIcone from "./../../assets/botaoIcone.png";
 function Consultas({ navigation }) {
   return (
     <ScrollView>
@@ -27,9 +30,9 @@ function Consultas({ navigation }) {
               <ConteudoCaixa>15-05-2002</ConteudoCaixa>
             </CaixaData>
             <CaixaNome>
-              <Icone></Icone>
+              <Icone source={botaoIcone} />
               <ConteudoCaixa>Consulta de Rotina</ConteudoCaixa>
-              <Icone></Icone>
+              <Icone source={botaoIcone} />
             </CaixaNome>
             <CaixaHora>
               <ConteudoCaixa>20:20</ConteudoCaixa>
@@ -41,28 +44,31 @@ function Consultas({ navigation }) {
               <ConteudoCaixa>15-05-2002</ConteudoCaixa>
             </CaixaData>
             <CaixaNome>
-              <Icone></Icone>
+              <Icone source={botaoIcone} />
               <ConteudoCaixa>Consulta de Rotina</ConteudoCaixa>
-              <Icone></Icone>
+              <Icone source={botaoIcone} />
             </CaixaNome>
             <CaixaHora>
               <ConteudoCaixa>20:20</ConteudoCaixa>
             </CaixaHora>
           </CaixaConsulta>
         </ViewConsultas>
-        <Botao
-          width="42%"
-          height="50px"
-          backgroundColor="#E9EBFC"
-          borderRadius="3px"
-          borderColor="#151B57"
-          borderWidth="1px"
-          boxShadow="0px 4px 4px rgba(0, 0, 0, 0.3)"
-        >
-          <ConteudoBotao fontSize="15px" color="#ffffff">
-            Marcar Nova Consulta
-          </ConteudoBotao>
-        </Botao>
+        <BotaoView>
+          <BotaoCaixa
+            width="45%"
+            height="40px"
+            backgroundColor="#E9EBFC"
+            borderRadius="3px"
+            borderColor="#000000"
+            borderWidth="1px"
+            boxShadow="0px 4px 4px rgba(0, 0, 0, 0.3)"
+          >
+            <ConteudoBotao fontSize="12px" color="#151B57" width="90%">
+              Marcar Nova Consulta
+            </ConteudoBotao>
+            <BotaoIcone source={botaoIcone} />
+          </BotaoCaixa>
+        </BotaoView>
       </Body>
     </ScrollView>
   );
--- src/Pages/Consultas/Styles.js
+++ src/Pages/Consultas/Styles.js
@@ -6,8 +6,8 @@ export const Body = styled.View`
   width: 100%;
   height: 100%;
   background-color: #ffffff;
-  padding-right:10%;
-  padding-left:10%;
+  padding-right: 5%;
+  padding-left: 5%;
 `;
 
 export const ViewConsultas = styled.View`
@@ -17,11 +17,14 @@ export const ViewConsultas = styled.View`
   align-items: center;
   width: 100%;
   margin-bottom: 5%;
-  margin-top: 7%;
   border-style: solid;
   border-radius: 6px;
-  border-color: rgba(0, 0, 0, 0.15);
-  border-width: 3px;
+  border: #151b57;
+  border-width: 2px;
+  padding-left: 10px;
+  padding-right: 10px;
+  padding-top: 6%;
+  padding-bottom: 1%;
 `;
 
 export const CaixaConsulta = styled.View`
@@ -29,35 +32,58 @@ export const CaixaConsulta = styled.View`
   flex-direction: row;
   justify-content: space-around;
   align-items: center;
-  width: 90%;
-  margin-bottom: 5%;
-  margin-top: 7%;
-  border-style: solid;
-  border-radius: 6px;
-  border-color: rgba(0, 0, 0, 0.15);
-  border-width: 3px;
-
-`
+  width: 100%;
+  border-radius: 5px;
+  margin-bottom: 4%;
+  background-color: #e1e4ff;
+  height: 38px;
+`;
 
 export const CaixaData = styled.View`
-
-`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 25%;
+  border-style: solid;
+  border: #bbc0f4;
+  border-width: 2.5px;
+  height: 100%;
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+`;
 
 export const CaixaNome = styled.View`
-
-`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 50%;
+  border-style: solid;
+  border: #bbc0f4;
+  border-top-width: 2.5px;
+  border-bottom-width: 2.5px;
+  height: 100%;
+`;
 
 export const CaixaHora = styled.View`
-
-`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 25%;
+  border-style: solid;
+  border: #bbc0f4;
+  border-width: 2.5px;
+  height: 100%;
+  border-top-right-radius: 5px;
+  border-bottom-right-radius: 5px;
+`;
 
 export const ConteudoCaixa = styled.Text`
+  color: #151b57;
+  font-size: 13px;
+`;
 
-`
-
-export const Icone = styled.Image`
-
-`
+export const Icone = styled.Image``;
 
 export const CaixaTitulo = styled.View`
   display: flex;
@@ -71,3 +97,31 @@ export const CaixaTitulo = styled.View`
 export const Titulo = styled.Text`
   font-size: 30px;
 `;
+export const BotaoView = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  width: 100%;
+  margin-bottom: 5%;
+`;
+export const BotaoCaixa = styled.TouchableOpacity`
+  /*Posicao */
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding-left: 2%;
+  padding-right: 5%;
+  /*Medidas*/
+  margin-top: 2%;
+  height: ${(props) => props.height};
+  width: ${(props) => props.width};
+  /*cor e borda do botao*/
+  overflow: hidden;
+  background-color: ${(props) => props.backgroundColor};
+  border-style: solid;
+  border-radius: ${(props) => props.borderRadius};
+  border-color: ${(props) => props.borderColor};
+  border-width: ${(props) => props.borderWidth};
+  box-shadow: ${(props) => props.boxShadow};
+`;
+export const BotaoIcone = styled.Image``;
