6b4c93a9ae701969bec92314df939594e59c79f7
Adrianus Alexsander <adrianusvieira@cpejr.com.br>
2022-04-26 23:40:59 -0300

    feat: Começando a estilizar o front

--- src/Pages/Consultas/Consultas.js
+++ src/Pages/Consultas/Consultas.js
@@ -1,27 +1,70 @@
 import React from "react";
-import { Text, View, Button } from "react-native";
+import { ScrollView } from "react-native";
+import {
+  Body,
+  CaixaTitulo,
+  Titulo,
+  ViewConsultas,
+  CaixaConsulta,
+  CaixaData,
+  CaixaNome,
+  CaixaHora,
+  ConteudoCaixa,
+  Icone,
+} from "./Styles";
+import { Botao, ConteudoBotao } from "../../styles/Botao";
 
 function Consultas({ navigation }) {
   return (
-    <View>
-      <Text> Consultas</Text>
-      <Button
-        title="Go to Cadastro"
-        onPress={() => navigation.navigate("Cadastro")}
-      />
-      <Button
-        title="Go to Login"
-        onPress={() => navigation.navigate("Login")}
-      />
-      <Button
-        title="Go to RecomendacoesPreConsulta"
-        onPress={() => navigation.navigate("RecomendacoesPreConsultaScreen")}
-      />
-      <Button
-        title="Go to SolicitarConsulta"
-        onPress={() => navigation.navigate("SolicitarConsultaScreen")}
-      />
-    </View>
+    <ScrollView>
+      <Body>
+        <CaixaTitulo>
+          <Titulo>Consultas Marcadas</Titulo>
+        </CaixaTitulo>
+        <ViewConsultas>
+          <CaixaConsulta>
+            <CaixaData>
+              <ConteudoCaixa>15-05-2002</ConteudoCaixa>
+            </CaixaData>
+            <CaixaNome>
+              <Icone></Icone>
+              <ConteudoCaixa>Consulta de Rotina</ConteudoCaixa>
+              <Icone></Icone>
+            </CaixaNome>
+            <CaixaHora>
+              <ConteudoCaixa>20:20</ConteudoCaixa>
+            </CaixaHora>
+          </CaixaConsulta>
+
+          <CaixaConsulta>
+            <CaixaData>
+              <ConteudoCaixa>15-05-2002</ConteudoCaixa>
+            </CaixaData>
+            <CaixaNome>
+              <Icone></Icone>
+              <ConteudoCaixa>Consulta de Rotina</ConteudoCaixa>
+              <Icone></Icone>
+            </CaixaNome>
+            <CaixaHora>
+              <ConteudoCaixa>20:20</ConteudoCaixa>
+            </CaixaHora>
+          </CaixaConsulta>
+        </ViewConsultas>
+        <Botao
+          width="42%"
+          height="50px"
+          backgroundColor="#E9EBFC"
+          borderRadius="3px"
+          borderColor="#151B57"
+          borderWidth="1px"
+          boxShadow="0px 4px 4px rgba(0, 0, 0, 0.3)"
+        >
+          <ConteudoBotao fontSize="15px" color="#ffffff">
+            Marcar Nova Consulta
+          </ConteudoBotao>
+        </Botao>
+      </Body>
+    </ScrollView>
   );
 }
 
---
+++ src/Pages/Consultas/Styles.js
@@ -0,0 +1,73 @@
+import styled from "styled-components/native";
+
+export const Body = styled.View`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background-color: #ffffff;
+  padding-right:10%;
+  padding-left:10%;
+`;
+
+export const ViewConsultas = styled.View`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin-bottom: 5%;
+  margin-top: 7%;
+  border-style: solid;
+  border-radius: 6px;
+  border-color: rgba(0, 0, 0, 0.15);
+  border-width: 3px;
+`;
+
+export const CaixaConsulta = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  width: 90%;
+  margin-bottom: 5%;
+  margin-top: 7%;
+  border-style: solid;
+  border-radius: 6px;
+  border-color: rgba(0, 0, 0, 0.15);
+  border-width: 3px;
+
+`
+
+export const CaixaData = styled.View`
+
+`
+
+export const CaixaNome = styled.View`
+
+`
+
+export const CaixaHora = styled.View`
+
+`
+
+export const ConteudoCaixa = styled.Text`
+
+`
+
+export const Icone = styled.Image`
+
+`
+
+export const CaixaTitulo = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin-top: 4%;
+  margin-bottom: 4%;
+`;
+
+export const Titulo = styled.Text`
+  font-size: 30px;
+`;
