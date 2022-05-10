96dfe2763511ff8cabe6e6db8c14c7a1c3cb9b0e
Maria <mariamsilva15@ufmg.br>
2022-05-10 00:47:17 -0300

    feat: alterei height e font-size input

--- src/styles/Input/Input.js
+++ src/styles/Input/Input.js
@@ -2,7 +2,7 @@ import styled from "styled-components/native";
 
 export const Input = styled.TextInput`
   width: ${(props) => props.width};
-  height: 40px;
+  height: 50px;
   background-color: #e4e6f4;
   border-radius: 5px;
   padding-left: 10px;
@@ -11,6 +11,6 @@ export const Input = styled.TextInput`
   border: #151b57;
   border-width: 1.5px;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.3);
-  font-size: 12px;
+  font-size: 15px;
 `;
 export default Input;
--- src/styles/InputMask/InputMask.js
+++ src/styles/InputMask/InputMask.js
@@ -4,7 +4,7 @@ import { TextInput } from "react-native";
 
 export const InputMask = styled(TextInputMask)`
   width: ${(props) => props.width};
-  height: 40px;
+  height: 50px;
   background-color: #e4e6f4;
   border-radius: 5px;
   padding-left: 10px;
@@ -13,6 +13,6 @@ export const InputMask = styled(TextInputMask)`
   border: #151b57;
   border-width: 1.5px;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.3);
-  font-size: 12px;
+  font-size: 15px;
 `;
 export default InputMask;
