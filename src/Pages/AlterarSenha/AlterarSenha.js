3751920d1d20578c8ef21843cba75f78b226b650
Adrianus Alexsander <adrianusvieira@cpejr.com.br>
2022-05-17 09:54:35 -0300

    feat:front responsivo

--- src/Pages/AlterarSenha/AlterarSenha.js
+++ src/Pages/AlterarSenha/AlterarSenha.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import { Dimensions } from "react-native";
 
 import {
@@ -15,55 +15,95 @@ import Input from "../../styles/Input/Input";
 import Botao from "../../styles/Botao/Botao";
 import ConteudoBotao from "../../styles/ConteudoBotao";
 
-function AlterarSenha() {
+function AlterarSenha({ navigation }) {
   const heightTela = `${Dimensions.get("window").height}px`;
 
   const [confirmado, setConfirmado] = useState(true);
 
   return (
-    <Body height={heightTela}>
-      <CaixaView paddingLeft="0%" paddingRight="0%">
-        <CaixaTitulo>
-          <Titulo fontSize="25px">Alterar Senha:</Titulo>
-        </CaixaTitulo>
-        {confirmado ? (
-          <CaixaInputs>
-            <CaixaTitulo>
-              <Titulo fontSize="15px">Comfirme sua senha atual:</Titulo>
-            </CaixaTitulo>
-            <Input placeholder="Senha Atual:"></Input>
-            <CaixaBotao>
-              <Botao
-                width="42%"
-                height="35px"
-                backgroundColor="#ffffff"
-                borderRadius="3"
-                borderColor="#DD0D0D"
-                borderWidth="1"
-              >
-                <ConteudoBotao width="100%" fontSize="15px" color="#000000">
-                  CANCELAR
-                </ConteudoBotao>
-              </Botao>
-              <Botao
-                width="42%"
-                height="35px"
-                backgroundColor="#434B97"
-                borderRadius="3"
-                borderColor="#151B57"
-                borderWidth="1"
-              >
-                <ConteudoBotao width="100%" fontSize="15px" color="#ffffff">
-                  CONFIRMAR
-                </ConteudoBotao>
-              </Botao>
-            </CaixaBotao>
-          </CaixaInputs>
-        ) : (
-          <CaixaInputs></CaixaInputs>
-        )}
-      </CaixaView>
-    </Body>
+    <ScrollView>
+      <Body height={heightTela}>
+        <CaixaView paddingLeft="0%" paddingRight="0%">
+          <CaixaTitulo>
+            <Titulo fontSize="25px">Alterar Senha:</Titulo>
+          </CaixaTitulo>
+          {confirmado ? (
+            <CaixaInputs>
+              <CaixaTitulo>
+                <Titulo fontSize="15px">Comfirme sua senha atual:</Titulo>
+              </CaixaTitulo>
+              <Input placeholder="Senha Atual:"></Input>
+              <CaixaBotao>
+                <Botao
+                  width="42%"
+                  height="35px"
+                  backgroundColor="#ffffff"
+                  borderRadius="3"
+                  borderColor="#DD0D0D"
+                  borderWidth="1"
+                  onPress={() => navigation.navigate("Perfil")}
+                >
+                  <ConteudoBotao width="100%" fontSize="15px" color="#000000">
+                    CANCELAR
+                  </ConteudoBotao>
+                </Botao>
+                <Botao
+                  width="42%"
+                  height="35px"
+                  backgroundColor="#434B97"
+                  borderRadius="3"
+                  borderColor="#151B57"
+                  borderWidth="1"
+                  onPress={() => setConfirmado(false)}
+                >
+                  <ConteudoBotao width="100%" fontSize="15px" color="#ffffff">
+                    CONFIRMAR
+                  </ConteudoBotao>
+                </Botao>
+              </CaixaBotao>
+            </CaixaInputs>
+          ) : (
+            <CaixaInputs>
+              <CaixaTitulo>
+                <Titulo fontSize="15px">Digite sua nova senha:</Titulo>
+              </CaixaTitulo>
+              <Input placeholder="Nova senha:"></Input>
+              <CaixaTitulo>
+                <Titulo fontSize="15px">Confirme sua nova senha:</Titulo>
+              </CaixaTitulo>
+              <Input placeholder="Confirmando sua nova senha:"></Input>
+              <CaixaBotao>
+                <Botao
+                  width="42%"
+                  height="35px"
+                  backgroundColor="#ffffff"
+                  borderRadius="3"
+                  borderColor="#DD0D0D"
+                  borderWidth="1"
+                  onPress={() => setConfirmado(true)}
+                >
+                  <ConteudoBotao width="100%" fontSize="15px" color="#000000">
+                    CANCELAR
+                  </ConteudoBotao>
+                </Botao>
+                <Botao
+                  width="42%"
+                  height="35px"
+                  backgroundColor="#434B97"
+                  borderRadius="3"
+                  borderColor="#151B57"
+                  borderWidth="1"
+                >
+                  <ConteudoBotao width="100%" fontSize="15px" color="#ffffff">
+                    CONFIRMAR
+                  </ConteudoBotao>
+                </Botao>
+              </CaixaBotao>
+            </CaixaInputs>
+          )}
+        </CaixaView>
+      </Body>
+    </ScrollView>
   );
 }
 
