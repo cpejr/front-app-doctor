c2c071a5a5532ad5407c6baa509686647e393e19
Adrianus Alexsander <adrianusvieira@cpejr.com.br>
2022-05-18 14:16:12 -0300

    feat: passando alterardados e perfil para o manager

--- src/services/ManagerService/managerService.js
+++ src/services/ManagerService/managerService.js
@@ -1,44 +1,46 @@
 import * as requesterService from "../RequesterService/requesterService";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export const GetDadosUsuario = async (emailUrl) => {
-    let dadosUsuario = {};
-    let dadosEndereco = {};
-  
-    await requesterService
-      .requisicaoDadosUsuario(emailUrl)
-      .then((res) => {
-        dadosUsuario = res.data;
-      })
-      .catch((error) => {
-        requisicaoErro(error);
-      });
-  
-    await requesterService
-      .requisicaoDadosEndereco(dadosUsuario)
-      .then((res) => {
-        dadosEndereco = res.data;
-      })
-      .catch((error) => {
-        requisicaoErro(error);
-      });
-    return { dadosEndereco, dadosUsuario };
-  };
+export const GetDadosUsuario = async () => {
+  const email = await AsyncStorage.getItem("@AirBnbApp:email");
+  let dadosUsuario = {};
+  let dadosEndereco = {};
 
-  export const UpdateDadosUsuario = async (
-    id_usuario,
-    id_endereco,
-    endereco,
-    estado
-  ) => {
-    await requesterService
-      .updateDadosUsuario(id_usuario, id_endereco, endereco, estado)
-      .then(() => {
-        alert("Usuário atualizado com sucesso.");
-      })
-      .catch((error) => {
-        requisicaoErro(error);
-        return false;
-      });
-  
-    return false;
-  };
+  await requesterService
+    .requisicaoDadosUsuario(email)
+    .then((res) => {
+      dadosUsuario = res.data;
+    })
+    .catch((error) => {
+      requisicaoErro(error);
+    });
+
+  await requesterService
+    .requisicaoDadosEndereco(dadosUsuario)
+    .then((res) => {
+      dadosEndereco = res.data;
+    })
+    .catch((error) => {
+      requisicaoErro(error);
+    });
+  return { dadosEndereco, dadosUsuario };
+};
+
+export const UpdateDadosUsuario = async (
+  id_usuario,
+  id_endereco,
+  endereco,
+  estado
+) => {
+  await requesterService
+    .updateDadosUsuario(id_usuario, id_endereco, endereco, estado)
+    .then(() => {
+      alert("Usuário atualizado com sucesso.");
+    })
+    .catch((error) => {
+      requisicaoErro(error);
+      return false;
+    });
+
+  return false;
+};
