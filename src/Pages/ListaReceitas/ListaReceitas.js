d9eba0ca33fbe1207f71c54d65b6d59ee7452c06
ThoshioCPE <thoshioneto@cperj.com.br>
2022-08-16 11:31:04 -0300

    feat: Scroll funcionando

--- src/Pages/ListaReceitas/ListaReceitas.js
+++ src/Pages/ListaReceitas/ListaReceitas.js
@@ -1,34 +1,38 @@
 import React, { useState, useEffect } from "react";
 import { sleep } from "../../utils/sleep";
-import { ActivityIndicator, Colors, Searchbar } from 'react-native-paper';
-import { Alert, ScrollView, Text, View, useWindowDimensions} from "react-native";
-import { 
-    ContainerBody, 
-    ContainerCima, 
-    ContainerTodasReceitas, 
-    ContainerReceitas, 
-    TituloReceitas, 
-    TextoData,
-    BarraPesquisa,
-    IconPesquisa,
-    InputPesquisa,
-    PaginaCarregando,
+import { ActivityIndicator, Colors, Searchbar } from "react-native-paper";
+import {
+  Alert,
+  ScrollView,
+  Text,
+  View,
+  useWindowDimensions,
+  FlatList,
+} from "react-native";
+import {
+  ContainerBody,
+  ContainerCima,
+  ContainerTodasReceitas,
+  ContainerReceitas,
+  TituloReceitas,
+  TextoData,
+  BarraPesquisa,
+  IconPesquisa,
+  InputPesquisa,
+  PaginaCarregando,
 } from "./Styles";
 import searchIcon from "../../assets/searchIcon.png";
 import Botao from "../../styles/Botao";
 import { Cores } from "../../variaveis";
 import * as managerService from "../../services/ManagerService/managerService";
 
-
 function ListaReceitas({ navigation }) {
+  const [receitas, setReceitas] = useState([]);
+  const [carregando, setCarregando] = useState(false);
+  const [busca, setBusca] = useState("");
+  const lowerBusca = busca.toLowerCase();
 
-  
-    const [ receitas, setReceitas ] = useState([]);
-    const [carregando, setCarregando] = useState(false);
-    const [busca, setBusca] = useState("");
-    const lowerBusca = busca.toLowerCase();
-
-  const onChangeBusca = busca => setBusca(busca);
+  const onChangeBusca = (busca) => setBusca(busca);
 
   async function pegandoReceitas() {
     setCarregando(true);
@@ -36,51 +40,52 @@ function ListaReceitas({ navigation }) {
     setReceitas(resposta.dadosReceitas[0].receita);
     setCarregando(false);
   }
-  
+
   useEffect(() => {
     pegandoReceitas();
   }, []);
 
-
   const receitasFiltradas = receitas.filter((receita) => {
-    if (lowerBusca === "") 
-      return receitas;
-    else 
-      return(
-        (receita?.titulo?.toLowerCase().includes(lowerBusca))
-      );
-    }); 
+    if (lowerBusca === "") return receitas;
+    else return receita?.titulo?.toLowerCase().includes(lowerBusca);
+  });
 
-    return(
+  return (
     <ContainerBody>
-        <ContainerCima>   
-            <BarraPesquisa>
-              <InputPesquisa
-                placeholder= "BUSCAR"
-                onChangeText={onChangeBusca}
-                value={busca} />
-              <IconPesquisa source = { searchIcon }/>
-              </BarraPesquisa>
-        </ContainerCima>
-        {carregando ? (
-            <PaginaCarregando>
-              <ActivityIndicator animating={true} color={Colors.black} />
-            </PaginaCarregando>) : ( 
-        <ContainerTodasReceitas>         
-        {receitasFiltradas?.map((value) => (
-            <ContainerReceitas key = {value.id}>
+      <ContainerCima>
+        <BarraPesquisa>
+          <InputPesquisa
+            placeholder="BUSCAR"
+            onChangeText={onChangeBusca}
+            value={busca}
+          />
+          <IconPesquisa source={searchIcon} />
+        </BarraPesquisa>
+      </ContainerCima>
+      {carregando ? (
+        <PaginaCarregando>
+          <ActivityIndicator animating={true} color={Colors.black} />
+        </PaginaCarregando>
+      ) : (
+        <ScrollView>
+          <ContainerTodasReceitas>
+            {receitasFiltradas?.map((value) => (
+              <ContainerReceitas key={value.id}>
                 <TituloReceitas>{value.titulo}</TituloReceitas>
                 <TextoData>
-                  {parseInt(value.data_criacao.slice(11, 13)) < 12 ? (
-                    value.data_criacao.slice(8, 10) + "/" + value.data_criacao.slice(5, 7) + "/" + value.data_criacao.slice(0, 4)
-                  ) : (
-                    value.data_criacao.slice(8, 10) + "/" + value.data_criacao.slice(5, 7) + "/" + value.data_criacao.slice(0, 4)
-                  )}
+                  {value.data_criacao.slice(8, 10) +
+                    "/" +
+                    value.data_criacao.slice(5, 7) +
+                    "/" +
+                    value.data_criacao.slice(0, 4)}
                 </TextoData>
-            </ContainerReceitas>))}
-        </ContainerTodasReceitas>)}
-    </ContainerBody> 
-    );
+              </ContainerReceitas>
+            ))}
+          </ContainerTodasReceitas>
+        </ScrollView>
+      )}
+    </ContainerBody>
+  );
 }
 
 export default ListaReceitas;
