2086f845ae37bace7c1f679d581ec78ce6f62934
ThoshioCPE <thoshioneto@cperj.com.br>
2022-08-18 10:36:52 -0300

    fix: pesquisando com acento

--- src/Pages/ListaFormularios/ListaFormularios.js
+++ src/Pages/ListaFormularios/ListaFormularios.js
@@ -55,8 +55,8 @@ function ListaFormularios({ navigation }) {
     if (lowerBusca === "") return formulariosPaciente;
     else
       return (
-        form?.titulo?.toLowerCase().includes(lowerBusca) ||
-        form?.tipo?.toLowerCase().includes(lowerBusca)
+        form?.titulo?.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g,'').includes(lowerBusca) ||
+        form?.tipo?.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g,'').includes(lowerBusca)
       );
   });
 
