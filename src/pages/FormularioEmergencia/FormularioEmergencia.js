1799086e59acc1449d22fdc2de682a4be171e5e2
osbarros <ceobarros@outlook.com>
2022-09-20 21:38:06 -0300

    feat: criação rota da pagina formulario de emergencia

---
+++ src/pages/FormularioEmergencia/FormularioEmergencia.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { Text, View, ScrollView, Alert } from "react-native";
+import Form from "react-native-jsonschema-form";
+
+function FormularioEmergencia () {
+  return(
+    <View>
+      <Text> OI </Text>
+    </View>
+  );
+}
+export default FormularioEmergencia
