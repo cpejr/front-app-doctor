a018cee7c5ec68ddd9ab484a30392a7145950030
osbarros <ceobarros@outlook.com>
2022-09-22 13:36:21 -0300

    feat: estilização da pág (menos do form)

--- src/pages/FormularioEmergencia/FormularioEmergencia.js
+++ src/pages/FormularioEmergencia/FormularioEmergencia.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { Text, View, ScrollView, Alert, TouchableOpacity, useWindowDimensions, } from "react-native";
+import { Text, View, ScrollView, Alert, TouchableOpacity, useWindowDimensions, ActivityIndicator, Dimensions} from "react-native";
 import Form from "react-native-jsonschema-form";
 import * as managerService from "../../services/ManagerService/managerService";
 import Icon from "react-native-vector-icons/Entypo";
 import { Cores } from "../../variaveis";
+import { HeaderFormularios, Titulo, Corpo, CorpoScroll, CaixaCima, CaixaFormulario, CaixaTitulo, CaixaBotao, Container, ConteudoBotaoUpload } from "./Styles";
+import Botao from "../../styles/Botao";
+import ConteudoBotao from "../../styles/ConteudoBotao";
 
 
 function FormularioEmergencia({ navigation }) {
@@ -16,8 +19,8 @@ function FormularioEmergencia({ navigation }) {
   const tamanhoIcone = width > 480 ? 20 : 25;
   const { width } = useWindowDimensions();
   const [schema, setSchema] = useState({});
-  const [schema2, setSchema2] = useState({})
   const [uiSchema, setUiSchema] = useState({});
+  const heightCorpo = `${Dimensions.get("window").height - 230}px`;
 
 
   async function GetFormulariosEmergenciaPaciente() {
@@ -28,6 +31,7 @@ function FormularioEmergencia({ navigation }) {
         setSchema(formularioPaciente.perguntas);
       }
     });
+    setCarregando(false);
   }
 
   useEffect(() => {
@@ -37,21 +41,79 @@ function FormularioEmergencia({ navigation }) {
 
 
   return (
-    <View>
-      <View>
+    <Container>
+      <HeaderFormularios>
         <TouchableOpacity onPress={() => navigation.push("Home")}>
           <Icon name="arrow-left" size={tamanhoIcone} color={Cores.azul} />
         </TouchableOpacity>
-      </View>
-      <View>
-        <Form
-          schema={schema}
-          uiSchema={uiSchema}
-        >
-          <></>
-        </Form>
-      </View>
-    </View>
+      </HeaderFormularios>
+      <CorpoScroll>
+        <Corpo height={heightCorpo}>
+          <CaixaCima>
+            <CaixaTitulo>
+              <Titulo
+                fontSize="24px"
+                color={Cores.azulEscuro}
+                marginBottom="8px"
+              >
+                {formularioPacienteDeEmergencia.titulo}
+              </Titulo>
+            </CaixaTitulo>
+            <CaixaFormulario>
+              {carregando ? (
+                <ActivityIndicator
+                  animating={true}
+                  color={Cores.azulEscuro}
+                />
+              ) : (
+                <View>
+                  <Form
+                    schema={schema}
+                    uiSchema={uiSchema}
+                  // widgets={widgets}}
+                  >
+                    <></>
+                  </Form>
+                </View>
+              )}
+            </CaixaFormulario>
+            <CaixaBotao>
+            <Botao
+                width="64%"
+                height="45px"
+                backgroundColor="green"
+                borderRadius="3px"
+                borderColor="black"
+                borderWidth="3px"
+                boxShadow="none"
+                marginTop="0"
+              >
+                <ConteudoBotaoUpload width="90%" fontSize="15px" color={Cores.azul}>
+                <ConteudoBotao width="90%" fontSize="15px" color={Cores.azul}>
+                  Upload de Arquivo
+                </ConteudoBotao>
+                  <Icon name = "upload" size = {tamanhoIcone} color={Cores.azul}></Icon>
+                </ConteudoBotaoUpload>
+              </Botao>
+              <Botao
+                width="32%"
+                height="40px"
+                backgroundColor={Cores.lilas[1]}
+                borderRadius="3px"
+                borderColor={Cores.azul}
+                borderWidth="3px"
+                boxShadow="none"
+                marginTop="0"
+              >
+                <ConteudoBotao width="90%" fontSize="15px" color={Cores.branco}>
+                  ENVIAR
+                </ConteudoBotao>
+              </Botao>
+            </CaixaBotao>
+          </CaixaCima>
+        </Corpo>
+      </CorpoScroll>
+    </Container>
 
   );
 }
---
+++ src/pages/FormularioEmergencia/Styles.js
@@ -0,0 +1,97 @@
+import styled from "styled-components/native";
+import { Cores } from "../../variaveis";
+
+export const Container = styled.View`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  overflow: scroll;
+  background-color: ${Cores.cinza[11]};
+`;
+
+export const HeaderFormularios = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding-top: 10px;
+  padding-bottom: 15px;
+  padding-left: 5px;
+  width: 100%;
+`;
+
+export const Titulo = styled.Text`
+  font-size: ${(props) => props.fontSize};
+  color: ${(props) => props.color};
+  margin-bottom: ${(props) => props.marginBottom ?? "0px"};
+  margin-left: 13px;
+`;
+
+export const CorpoScroll = styled.ScrollView`
+  display: flex;
+  width: 90%;
+  height: 100%;
+  background-color: ${Cores.cinza[11]};
+`;
+
+export const Corpo = styled.View`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: ${(props) => props.height};
+  padding-right: 5%;
+  padding-left: 5%;
+  flex-direction: column;
+  background-color: ${Cores.cinza[11]};
+`;
+
+export const CaixaCima = styled.View`
+  width: 100%;
+  height: 100%;
+  padding-bottom: 30%;
+  border-width: 1px;
+  border-color: ${Cores.azulEscuro};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  background-color: ${Cores.cinza[11]};
+`;
+
+export const CaixaTitulo = styled.View`
+  display: flex;
+  margin-top: 20px;
+  width: 90%;
+  align-items: center;
+`;
+
+export const CaixaFormulario = styled.View`
+  display: flex;
+  height: 50%;
+  margin-top: 30px;
+  width: 100%;
+  padding-right: 7%;
+  padding-left: 7%;
+  margin-bottom: 40%;
+`;
+
+export const CaixaBotao = styled.View`
+  width: 90%;
+  height: 95px;
+  display: flex;
+  justify-content: space-between;
+  background-color: ${Cores.cinza[11]};
+  align-items: center;
+`;
+
+export const ConteudoBotaoUpload = styled.View`
+  width: 100%;
+  padding-left: 5%;
+  padding-right: 10%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+
+
