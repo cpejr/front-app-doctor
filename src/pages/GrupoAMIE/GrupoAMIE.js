95166f58dbaaf7a2963d55e21a0cf1892bfcc7e5
GabrielliSilva <gabriellisilva@cpejr.com.br>
2022-10-17 23:20:31 -0300

    feat: construção inicial

--- src/pages/GrupoAMIE/GrupoAMIE.js
+++ src/pages/GrupoAMIE/GrupoAMIE.js
@@ -1,12 +1,82 @@
 import React from "react";
-import { Text, View } from "react-native";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  useWindowDimensions,
+  ScrollView,
+} from "react-native";
+import Icon from "react-native-vector-icons/Entypo";
+import { Cores } from "../../variaveis";
+import Botao from "../../styles/Botao";
+import {
+  Body,
+  CaixaSeta,
+  CaixaTitulo,
+  Titulo,
+  CaixaScroll,
+  CaixaDescricao,
+  CaixaBotao,
+  CaixaCentro,
+  Descricao,
+  TextoBotao,
+} from "./Styles";
 
 function GrupoAMIE() {
+  const { width } = useWindowDimensions();
+  const tamanhoIcone = width > 480 ? 20 : 25;
+  const larguraBotoesMaior = width < 600 ? "60%" : "50%";
+  const larguraBotoes = width < 330 ? "60%" : larguraBotoesMaior;
+
   return (
-    <View>
-      <Text>GrupoAMIE</Text>
-    </View>
+    <Body>
+      <CaixaSeta>
+        <TouchableOpacity>
+          <Icon
+            name="arrow-left"
+            size={tamanhoIcone}
+            /* color={Cores.azul} */   color = "green"  
+          />
+        </TouchableOpacity>
+      </CaixaSeta>
+      <CaixaTitulo>
+        <Titulo>Exame específico</Titulo>
+      </CaixaTitulo>
+      <CaixaCentro>
+        <ScrollView>
+          <CaixaScroll>
+            <CaixaDescricao>
+              <Descricao>
+                Você me ama? Perguntou Alice. — Não, não te amo! Respondeu o
+                Coelho Branco. Alice franziu a testa e juntou as mãos como fazia
+                sempre que se sentia ferida. —Vês? Retorquiu o Coelho Branco.
+                Agora vais começar a perguntar-te o que te torna tão imperfeita
+                e o que fizeste de mal para que eu não consiga amar-te pelo
+                menos um pouco. Sabes, é por esta razão que não te posso amar.
+
+              </Descricao>
+            </CaixaDescricao>
+          </CaixaScroll>
+        </ScrollView>
+      </CaixaCentro>
+      <CaixaBotao>
+        <Botao
+          width={larguraBotoes}
+          height="40px"
+          marginTop="0%"
+        /*   backgroundColor={Cores.cinza[11]} */
+        backgroundColor="green"
+          borderRadius="3px"
+          borderColor={Cores.azul}
+          borderWidth="2px"
+          boxShadow="0px 4px 4px rgba(0, 0, 0, 0.2)"
+        >
+          <TextoBotao>Agendar Exame Específico</TextoBotao>
+        </Botao>
+      </CaixaBotao>
+    </Body>
   );
 }
 
 export default GrupoAMIE;
+
