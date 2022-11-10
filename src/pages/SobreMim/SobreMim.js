eac165bc67893845079378af772289270a7022a6
ThoshioCPE <thoshioneto@cperj.com.br>
2022-11-10 01:26:15 -0300

    feat: finalizando a construção da página e a responsividade

--- src/pages/SobreMim/SobreMim.js
+++ src/pages/SobreMim/SobreMim.js
@@ -1,27 +1,64 @@
-import React from "react";
-import { Text, View, Image, ScrollView } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Image,
+  useWindowDimensions,
+} from "react-native";
 import {
   BotaoVoltar,
   CaixaBotao,
   CaixaImagem,
   CaixaTexto,
   ContainerScroll,
-  Corpo,
-  Imagem,
+  PaginaCarregando,
+  Texto,
   TituloTexto,
 } from "./Styles";
 import { Cores } from "../../variaveis";
 import IconeVoltar from "react-native-vector-icons/Feather";
 import GuilhermeMarquesFotoSobreMim from "./Imagens/GuilhermeMarquesFotoSobreMim.webp";
 import GuilhermeMarquesTemplateArteSobreMim from "./Imagens/GuilhermeMarquesTemplateArteSobreMim.webp";
+import { useFonts } from "expo-font";
+import { ActivityIndicator } from "react-native-paper";
 
-//importar fonte e descobrir a dimensão da tela.
-function SobreMim() {
+function SobreMim({ navigation }) {
+  const [carregando, setCarregando] = useState(true);
+  const [larguraImagem, setLarguraImagem] = useState();
+  const [alturaImagem, setAlturaImagem] = useState();
+
+  const larguraTela = useWindowDimensions().width;
+
+  function SetandoDimensoesImagem() {
+    setLarguraImagem(100 * larguraTela);
+    setAlturaImagem(((27 * 0.88) / 47) * larguraTela);
+  }
+
+  useEffect(() => {
+    setCarregando(true);
+    SetandoDimensoesImagem();
+    setCarregando(false);
+  }, [larguraTela]);
+
+  const [loaded] = useFonts({
+    BarlowSemibold: require("../../assets/fonts/Barlow-SemiBold.ttf"),
+    BarlowLight: require("../../assets/fonts/Barlow-Light.ttf"),
+  });
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <ContainerScroll>
+      {carregando ? (
+        <>
+          <PaginaCarregando>
+            <ActivityIndicator animating={true} color={Colors.black} />
+          </PaginaCarregando>
+        </>
+      ) : (
       <View style={{ alignItems: "center" }}>
         <CaixaBotao>
-          <BotaoVoltar>
+          <BotaoVoltar onPress={() => navigation.navigate("Home")}>
             <IconeVoltar
               name="arrow-left-circle"
               size={40}
@@ -31,34 +68,151 @@ function SobreMim() {
         </CaixaBotao>
 
         <CaixaImagem>
-          <Imagem
+          <Image
+            style={{
+              width: larguraImagem,
+              height: alturaImagem,
+              borderRadius: 3,
+              resizeMode: "contain",
+            }}
             source={GuilhermeMarquesTemplateArteSobreMim}
             alt={
               "Aqui existe uma imagem da logomarca do doutor Guilherme Marques" +
               30
             }
-          ></Imagem>
+          ></Image>
         </CaixaImagem>
 
         <CaixaTexto>
-          <TituloTexto>Quem sou</TituloTexto>
+          <TituloTexto fontFamily="BarlowSemibold">Quem sou</TituloTexto>
+          <Texto fontFamily="BarlowLight" textAlign="justify">
+            {"\n"}
+            {"\t"}
+            {"\t"}
+            {"\t"}
+            {"\t"}Com formação em Neurologia e Neurofisiologia Clínica, atuo na
+            investigação, diagnóstico e tratamento de variadas doenças e
+            transtornos neurológicos. Após especialização no Hospital das
+            Clínicas da Universidade de São Paulo - USP - Ribeirão Preto,
+            retorno a Belo Horizonte para atuação em neurologia geral tanto
+            hospitalar de urgência quanto ambulatorial, trazendo o conhecimento
+            e experiência adquiridos em um dos maiores centros de Neurologia do
+            país.
+            {"\n"}
+            {"\n"}
+            {"\t"}
+            {"\t"}
+            {"\t"}
+            {"\t"}O atendimento no pronto-socorro é focado no diagnóstico e
+            tratamento rápido de situações associadas a grande sofrimento ou
+            risco de lesões, sequelas ou morte. No cenário de urgência, faço
+            parte das equipes dos Hospitais Mater Dei de BH e de Betim/Contagem.
+            {"\n"}
+            {"\n"}
+            {"\t"}
+            {"\t"}
+            {"\t"}
+            {"\t"}Já as condições neurológicas crônicas, de longa duração, ou
+            que se repetem com frequência e trazem sofrimento e desconforto ao
+            paciente, merecem ter acompanhamento regular no consultório, em
+            nível ambulatorial. É nesse ambiente, com o tempo reservado e
+            atendimento individualizado, que é possível desvendar como a doença
+            interage com os fatores sociais e psicológicos em um dado paciente
+            e, assim, personalizar o tratamento para obter o melhor resultado
+            possível.
+            {"\n"}
+            {"\n"}
+            {"\t"}
+            {"\t"}
+            {"\t"}
+            {"\t"}Nem sempre podemos curar, mas podemos aliviar quase sempre e,
+            com certeza, podemos confortar todas vezes. A minha filosofia de
+            atendimento envolve a realização de uma consulta mais prolongada,
+            pois somente assim é possível dar a atenção que é necessária ao
+            paciente e aos familiares, e assim estabelecer o diagnóstico
+            correto, o tratamento eficaz, além de poder esclarecer todas as
+            dúvidas e aliviar todos os receios que sempre se apresentam quando
+            nossa saúde está sob risco.
+          </Texto>
+          <Texto fontFamily="BarlowLight" textAlign="center">
+            {"\n"}Dr. Guilherme Marques
+            {"\n"}CRM-MG 56.888
+            {"\n"}
+            {"\n"}Neurologia - RQE Nº: 38808
+            {"\n"}Neurofisiologia Clínica - RQE Nº: 41054
+            {"\n"}Clínica Médica - RQE Nº: 40153
+            {"\n"}Mestre em Ciências pela USP
+            {"\n"}
+            {"\n"}Contato do consultório particular:
+            {"\n"}Rua Matias Cardoso, 129 - Pilotis, Santo Agostinho - Belo
+            Horizonte
+          </Texto>
+          <Texto fontFamily="BarlowSemibold" textAlign="center">
+            {"\n"}Tel.: (31) 97248-8306
+          </Texto>
+          <Texto fontFamily="BarlowLight" textAlign="center">
+            Secretárias: Larissa e Júlia
+          </Texto>
         </CaixaTexto>
 
         <CaixaImagem>
-          <Imagem
+          <Image
+            style={{
+              width: larguraImagem,
+              height: alturaImagem,
+              borderRadius: 3,
+              resizeMode: "contain",
+            }}
             source={GuilhermeMarquesFotoSobreMim}
             alt={
               "Aqui existe uma foto do doutor Guilherme Marques em seu consultório" +
               30
             }
-          ></Imagem>
+          ></Image>
         </CaixaImagem>
 
         <CaixaTexto>
-          <TituloTexto>Minha Experiência</TituloTexto>
+          <TituloTexto fontFamily="BarlowSemibold">
+            Minha Experiência
+          </TituloTexto>
+          <Texto fontFamily="BarlowLight" textAlign="justify">
+            {"\n"}Graduação em Medicina pela Faculdade de Medicina da
+            Universidade Federal de Minas Gerais (UFMG), 2006 a 2012
+            {"\n"}
+            {"\n"}Residência Médica em Neurologia, Hospital das Clínicas da
+            UFMG, 2014 a 2017
+            {"\n"}
+            {"\n"}Título de especialista em Clínica Médica, Sociedade Brasileira
+            de Clínica Médica, 2017
+            {"\n"}
+            {"\n"}Título de especialista em Neurologia, Academia Brasileira de
+            Neurologia, 2017
+            {"\n"}
+            {"\n"}Residência Médica em Neurofisiologia Clínica, Hospital das
+            Clínicas de Ribeirão Preto - Universidade de São Paulo - USP, 2017 a
+            2018
+            {"\n"}
+            {"\n"}Título de Especialista em Neurofisiologia Clínica pela
+            Sociedade Brasileira de Neurofisiologia Clínica - SBNC, 2018.
+            {"\n"}
+            {"\n"}Mestre em Neurologia, Hospital das Clínicas de Ribeirão Preto
+            - Universidade de São Paulo - USP, em andamento, 2018-2019
+            {"\n"}
+            {"\n"}Membro fundador do Grupo AMIE - Avaliação e Manejo Integrado
+            das Epilepsias
+            {"\n"}
+            {"\n"}Membro da Liga Mineira do Sono
+            {"\n"}
+            {"\n"}Médico Neurofisiologista (eletroencefalografia) - Clínica
+            Conrad - Belo Horizonte
+            {"\n"}
+            {"\n"}Médico do Sono - Laboratório do Sono - Belo Horizonte
+            {"\n"}
+            {"\n"}Chefe da Clínica de Neurologia do Hospital da Polícia Militar
+            de Minas Gerais
+          </Texto>
         </CaixaTexto>
-        
-      </View>
+      </View>)}
     </ContainerScroll>
   );
 }
--- src/pages/SobreMim/Styles.js
+++ src/pages/SobreMim/Styles.js
@@ -7,21 +7,21 @@ export const ContainerScroll = styled.ScrollView`
   background-color: ${Cores.branco};
 `;
 
-export const Corpo = styled.View`
-  display: flex;
+export const PaginaCarregando = styled.View`
   flex: 1;
-  align-items: center;
   justify-content: center;
-  flex-direction: column;
+  align-items: center;
+  padding-top: 70%;
 `;
 
 export const CaixaBotao = styled.View`
     display: flex;
     flex: 1;
     width: 100%;
-    height: auto;
-    padding-left: 5%;
-    margin-top: 4%;
+    height: 50px;
+    padding-left: 6%;
+    margin-top: 20px;
+    margin-bottom: 10px;
 `;
 
 export const BotaoVoltar = styled.TouchableOpacity`
@@ -29,19 +29,13 @@ export const BotaoVoltar = styled.TouchableOpacity`
     height: 44px;
 `;
 
-//tornar dimensões responsivas
 export const CaixaImagem = styled.View`
     display: flex;
     flex: 1;
     align-items: center;
     justify-content: center;
-`;
-
-export const Imagem = styled.Image`
-  width: 423px;
-  height: 243px;
-  resize-mode: center;
-  border-radius: 3px;
+    width: 88%;
+    border-radius: 3px;
 `;
 
 export const CaixaTexto = styled.View`
@@ -50,16 +44,30 @@ export const CaixaTexto = styled.View`
     align-items: center;
     justify-content: center;
     width: 88%;
+    height: auto;
     background-color: ${Cores.cinza[9]};
-    height: 200px;
     flex-direction: column;
     border: 2px solid ${Cores.azul};
     border-radius: 3px;
+    padding-top: 20px;
+    padding-left: 30px;
+    padding-right: 30px;
+    padding-bottom: 35px;
+    margin-top: 5%;
+    margin-bottom: 5%;
 `;
 
-//add fonte
 export const TituloTexto = styled.Text`
-  font-size: 18px;
+  font-family: ${(props) => props.fontFamily};
+  text-align: center;
+  font-size: 25px;
   font-weight: 500;
   color: ${Cores.azulEscuro};
+`;
+
+export const Texto = styled.Text`
+  font-family: ${(props) => props.fontFamily};
+  text-align: ${(props) => props.textAlign};
+  font-size: 18px;
+  color: ${Cores.preto};
 `;
