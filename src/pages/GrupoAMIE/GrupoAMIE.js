7d3815af304f78cfdfff6355b0b0e16a4537133a
hmcfelipe <felipehmc14@hotmail.com>
2022-11-17 17:34:16 -0300

    feat: video android 9.1

--- src/pages/GrupoAMIE/GrupoAMIE.js
+++ src/pages/GrupoAMIE/GrupoAMIE.js
@@ -1,11 +1,36 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState, useCallback, useRef } from "react";
+import { Button, View, Text, Alert } from "react-native";
+import YoutubePlayer from "react-native-youtube-iframe";
+import {WebView} from "react-native-webview";
 
 function GrupoAMIE() {
+  const [playing, setPlaying] = useState(false);
+
+  const onStateChange = useCallback((state) => {
+    if (state === "ended") {
+      setPlaying(false);
+      Alert.alert("video has finished playing!");
+    }
+  }, []);
+
+  const togglePlaying = useCallback(() => {
+    setPlaying((prev) => !prev);
+  }, []);
   return (
     <View>
       <Text>GrupoAMIE</Text>
-    </View>
+    <YoutubePlayer
+    javaScriptCanOpenWindowsAutomatically={true}
+    mediaPlaybackRequiresUserAction={true}
+    javaScriptEnabled={true}
+    width={350}
+    height={300}
+    play={playing}
+    videoId={"z-0AMvyarjE"}
+    onChangeState={onStateChange}
+  />
+  <Button title={playing ? "pause" : "play"} onPress={togglePlaying} />
+  </View>
   );
 }
 
