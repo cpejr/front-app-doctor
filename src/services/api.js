92b22f628df7a1560371a624610f2bac94122980
GabrielliSilva <gabriellisilva@cpejr.com.br>
2022-12-06 09:23:43 -0300

    feat: setando a api dinamicamente

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,27 @@
 import axios from "axios";
+import Constants from 'expo-constants'
 
 /* Endereços para cada emulador/simulador:
  ** Genymotion:              http://10.0.3.2:3333/
  ** Emulador Android Studio: http://10.0.2.2:3333/
  ** Simulador IOS:           http://localhost:3333/
  */
+
+const productionHost = "urldeproducao";
+const productionPort = "0000";
+let uri = `https://${productionHost}:${productionPort}`;
+
+
+if(__DEV__) {
+  const developmentHost = Constants.manifest.hostUri.split(":")[0]
+  const developmentPort = 3333;
+  
+  uri = `http://${developmentHost}:${developmentPort}`;
+}
+
 const api = axios.create({
-  baseURL: "http://192.168.100.161:3333/",
+  baseURL: uri,
 });
+
 
 export default api;
